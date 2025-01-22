d9f56933558a4c83adfe8636344409025de122e7
Matthew Bach II <matthew.bach.gamer@gmail.com>
2025-01-22 18:05:52 -0500

    Personalization - Big Update, Resume

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -1,23 +1,18 @@
 import {
   IconBrandLinkedin,
-  IconBrandTwitter,
-  IconBrandYoutube,
+  /*IconBrandTwitter,
+  IconBrandYoutube,*/
 } from "@tabler/icons-react";
 
 export const socials = [
   {
-    href: "https://twitter.com/mannupaaji",
-    label: "Twitter",
-    icon: IconBrandTwitter,
-  },
-  {
-    href: "https://linkedin.com/in/manuarora28",
+    href: "https://www.linkedin.com/in/matthew-bach-ii/",
     label: "LinkedIn",
     icon: IconBrandLinkedin,
-  },
+  }/*,
   {
     href: "https://youtube.com/maninthere",
     label: "YouTube",
     icon: IconBrandYoutube,
-  },
+  }*/,
 ];
