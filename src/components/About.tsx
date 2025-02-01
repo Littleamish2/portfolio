72650b7c36baf237aa5bab0d4750e98660d15318
Matthew Bach II <matthew.bach.gamer@gmail.com>
2025-02-01 17:02:57 -0500

    Changed from map to individual elements.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,7 @@ export default function About() {
   return (
     <div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-10">
-        {images.map((image, index) => (
+        {/*{images.map((image, index) => (
           <motion.div
             key={image}
             initial={{
@@ -38,36 +38,36 @@ export default function About() {
               className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
             />
           </motion.div>
-        ))}
-        {/* 
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
-        // />
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
-        // />
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
-        // />
-        // <Image
-        //   src="https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-        //   width={200}
-        //   height={400}
-        //   alt="about"
-        //   className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
-        // /> */}
+        ))}*/}
+         
+        <Image
+          src="@/Sabre2_PCB.png"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
+        />
+        <Image
+          src="@/CentralPark.png"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        />
+        <Image
+          src="@/NASA_Lab.png"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        />
+        <Image
+          src="@/SetPicture.png"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        /> 
       </div>
 
       <div className="max-w-4xl">
@@ -79,7 +79,7 @@ export default function About() {
         </Paragraph>
 
         <Paragraph className=" mt-4">
-          I currently specialize in Python (NumPy, matplotlib), C, C++, 
+          I currently specialize in Python (NumPy, Matplotlib), C, C++, 
           and Java. I also have experience in several design tools like AutoCAD, SketchUp, 
           Bambu Lab, Altium Designer, Quartus Prime, and Multisim. My focus going forward 
           will be to improve my cloud development skills and explore more of electrical engineering.
